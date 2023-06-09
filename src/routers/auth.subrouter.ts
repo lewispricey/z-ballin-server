bb45e06324e5be4af0648fd47d6f2e5c86cf5e52
Lewis Price <1995ldp@gmail.com>
2023-06-09 17:33:52 +0100

    fix filenaming to match repo standards

--- src/routers/auth.subrouter.ts
+++ src/routers/auth.subrouter.ts
@@ -1,8 +1,10 @@
 import express, { Router } from "express";
-import postRegister from "../controllers/auth/postRegister";
+import postRegister from "../controllers/auth/postRegister.controller";
+import postLogin from "../controllers/auth/postLogin.controller";
 
 const authRouter: Router = express.Router();
 
 authRouter.post("/register", postRegister);
+authRouter.post("/login", postLogin);
 
 export default authRouter;
