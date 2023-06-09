e45e19cc9ec7716126914a2e520513ae68ab54e6
Lewis Price <1995ldp@gmail.com>
2023-06-09 09:36:04 +0100

    updates custom error to use code key over status

--- src/errors/customError.ts
+++ src/errors/customError.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 
 const customError = (
-  error: { status: number; msg: string },
+  error: { code: number; msg: string },
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
   if (error.msg) {
-    response.status(error.status).send({ msg: error.msg });
+    response.status(error.code).send({ msg: error.msg });
   }
 };
 
